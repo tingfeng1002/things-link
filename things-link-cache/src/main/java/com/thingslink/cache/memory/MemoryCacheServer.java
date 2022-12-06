0a2070f785eaebbbf013d3479aa90d49175c987c
wangxiao <wxiao1002@yeah.net>
2022-12-06 14:38:45 +0800

    feature(*) base

---
+++ things-link-cache/src/main/java/com/thingslink/cache/memory/MemoryCacheServer.java
@@ -0,0 +1,8 @@
+package com.thingslink.cache.memory;
+
+/**
+ * @author wang xiao
+ * date 2022/12/6
+ */
+public class MemoryCacheServer {
+}
---
+++ things-link-cache/src/main/java/com/thingslink/cache/redis/RedisCacheServer.java
@@ -0,0 +1,8 @@
+package com.thingslink.cache.redis;
+
+/**
+ * @author wang xiao
+ * date 2022/12/6
+ */
+public class RedisCacheServer {
+}
