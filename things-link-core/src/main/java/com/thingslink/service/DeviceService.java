0a8e3a097ea004bca75844c4176a02dbe791c0f2
wangxiao <wxiao1002@yeah.net>
2022-12-08 20:28:19 +0800

    doc(*) update doc

--- things-link-core/src/main/java/com/thingslink/service/DeviceService.java
+++ things-link-core/src/main/java/com/thingslink/service/DeviceService.java
@@ -1,5 +1,9 @@
-package com.thingslink.service;/**
+package com.thingslink.service;
+
+/**
+ * 设备 service
  * @author wang xiao
  * date 2022/12/8
- */   public interface DeviceService {
+ */
+public interface DeviceService {
 }
