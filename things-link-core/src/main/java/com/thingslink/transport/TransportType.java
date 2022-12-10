0ca8125d92f5208bffc657dba619f32dc27803c7
wangxiao <wxiao1002@yeah.net>
2022-12-10 21:37:03 +0800

    feature(*) core

---
+++ things-link-core/src/main/java/com/thingslink/transport/TransportType.java
@@ -0,0 +1,19 @@
+package com.thingslink.transport;
+
+/**
+ * 传输类型
+ * @author wang xiao
+ * date 2022/12/9
+ */
+public enum TransportType {
+
+    /**
+     * MQTT protocol
+     */
+    MQTT,
+
+    /**
+     * HTTP protocol
+     */
+    HTTP
+}
